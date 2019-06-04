80da08a29b353a8d3ce3c3038e56617e2e527552
Raeshmithaa <51357144+Raeshmithaa@users.noreply.github.com>
2019-06-04 14:38:59 +0530

    Create fro.py

---
+++ fro.py
@@ -0,0 +1,5 @@
+char=input("enter the letter")
+if(char=='a'or char=='e'or char=='i'or char=='o'or char=='u'):     
+    print("vowel")
+elif(char!='a'or char!='e'or char!='i'or char!='o'or char!='u'):
+    print("invalid")    
