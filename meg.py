179a656355904043d90b6ab28f2e56d5e4f7efeb
Raeshmithaa <51357144+Raeshmithaa@users.noreply.github.com>
2019-06-04 16:06:24 +0530

    Create meg.py

---
+++ meg.py
@@ -0,0 +1,9 @@
+a=int(input("enter the 1stvalue"))
+b=int(input("enter the 2ndvalue"))
+c=int(input("enter the 3ndvalue"))
+if(a>b) and (a>c):
+    print("a is greater")
+elif(b>a) and (b>c):
+    print("b is greater") 
+else:
+    print("c is greater")
