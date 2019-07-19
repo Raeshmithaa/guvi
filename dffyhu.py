46489d5fb86438d58d975b94801517fb7bdb221e
Raeshmithaa <51357144+Raeshmithaa@users.noreply.github.com>
2019-07-19 12:49:42 +0530

    Update dffyhu.py

--- dffyhu.py
+++ dffyhu.py
@@ -1,6 +1,6 @@
 if (r % 4) == 0:
    if (r % 100) == 0:
        if (r % 400) == 0:
-           print("r is a leapyear")
+           print("yes")
        else:
-           print("r is not a leap year")
+           print("no")
