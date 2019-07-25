230b893701e36b6ebf1d92d0ec83f7967b3d6a2c
Raeshmithaa <51357144+Raeshmithaa@users.noreply.github.com>
2019-07-25 16:45:22 +0530

    Create 14.py

---
+++ 14.py
@@ -0,0 +1,13 @@
+ra,es=map(int,input().split())
+y=list(map(int,input().split()))
+ra=[]
+y.insert(0,0)
+for a in range(es):
+     hh=[]
+     ww=0
+     x9,y9=map(int,input().split())
+     for b in range(x9,y9+1):         
+         ww^=l6[b]     
+     ra.append(ww)
+for c in ra:
+    print(c)
